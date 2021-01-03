cb4cf2e713dbee638555815e6327b2509b17f9f5
akhpp <74254758+akhpp@users.noreply.github.com>
2021-01-03 23:06:19 +0530

    Commands

---
+++ commands.py
@@ -0,0 +1,42 @@
+# ForwardTagRemoverBot - A Telegram Bot To Hide Forward Source
+#Copyright (C) 2020 by Preet <https://github.com/akhpp>
+#ForwardTagRemoverBot is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#ForwardTagRemoverBot is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup,ParseMode
+from config import Config
+
+#Inline Keyboard Button
+keyboard = [
+[
+ InlineKeyboardButton("Source Code", url=Config.SOURCE)
+],
+]
+
+#The Keyboard On Up👆
+reply_markup = InlineKeyboardMarkup(keyboard)
+
+#Send Start Message
+def startMessage(update,context):
+ try:
+  update.message.reply_text(Config.START_TEXT.format(update.message.from_user.full_name,update.message.chat.id),reply_markup=reply_markup,
+parse_mode=ParseMode.MARKDOWN)
+ except Exception as e:
+ 	update.message.reply_text(e)
+
+#Help Message
+def helpMessage(update,context):
+ try:
+   update.message.reply_text(Config.HELP_TEXT,reply_markup=reply_markup,parse_mode=ParseMode.MARKDOWN)
+ except Exception as e:
+  	update.message.reply_text(e)
